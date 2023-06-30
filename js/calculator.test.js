1ae1575cb58b3491a15049f90c92584202f4037f
daimiao.chen@MBP <daniel-daimiao-chen@outlook.com>
2023-06-30 10:40:58 -0400

    Unit Test: reset mock function before each test case
    The mock function will record each result that have be happend.
    The function toHaveBeenCalled() will seach the result from the all
    records.
    This is make sense, because sometime the mock will be call mulitipl
    times with one time. And we expect to test then.
    So we need to reset the mockFn each time.
    
    And I don't want the unit test print too many log.

--- js/calculator.test.js
+++ js/calculator.test.js
@@ -3,61 +3,53 @@ import {Calculator as calc} from './calculator'
 import {jest} from '@jest/globals';
 
 function testBasicOperator(cal, output) {
-  test('evaluate with empty', () => {
+  test('test the basic operator', () => {
+    output.mockReset();
     /* evaluate with '' and output should not be call */
     cal.evaluate('');
     expect(output).not.toHaveBeenCalled();
-  });
 
-  test('evaluate with 1+2', () => {
+    output.mockReset();
     /* evaluate with '1+2' and output should be call with '3' */
     cal.evaluate('1+2');
     expect(output).toHaveBeenCalledWith('3');
-  });
 
-  test('evaluate with 2-1', () => {
+    output.mockReset();
     /* evaluate with '2-1' and output should be call with '1' */
     cal.evaluate('2-1');
     expect(output).toHaveBeenCalledWith('1');
-  });
 
-  test('evaluate with 2*3', () => {
+    output.mockReset();
     /* evaluate with '2*3' and output should be call with '6' */
     cal.evaluate('2*3');
     expect(output).toHaveBeenCalledWith('6');
-  });
 
-  test('evaluate with 4/2', () => {
+    output.mockReset();
     /* evaluate with '4/2' and output should be call with '2' */
     cal.evaluate('4/2');
     expect(output).toHaveBeenCalledWith('2');
-  });
 
-  test('evaluate with 2/0', () => {
+    output.mockReset();
     /* evaluate with '2/0' and output should be call with 'Infinity' */
     cal.evaluate('2/0');
     expect(output).toHaveBeenCalledWith('Infinity');
-  });
 
-  test('evaluate with 2%3', () => {
+    output.mockReset();
     /* evaluate with '2%3' and output should be call with '2' */
     cal.evaluate('2%3');
     expect(output).toHaveBeenCalledWith('2');
-  });
 
-  test('evaluate with 2**3', () => {
+    output.mockReset();
     /* evaluate with '2**3' and output should be call with '8' */
     cal.evaluate('2**3');
     expect(output).toHaveBeenCalledWith('8');
-  });
 
-  test('evaluate with 2&1', () => {
+    output.mockReset();
     /* evaluate with '2&1' and output should be call with '0' */
     cal.evaluate('2&1');
     expect(output).toHaveBeenCalledWith('0');
-  });
 
-  test('evaluate with error', () => {
+    output.mockReset();
     /* evaluate with 'yoiuou' and output should be call with 'error' */
     cal.evaluate('yoiuou');
     expect(output).toHaveBeenCalledWith('error');
@@ -76,4 +68,8 @@ describe('calculator', () => {
     cal.evaluate('clear');
     expect(updateResult).toHaveBeenCalledWith('');
   });
+
+  /* make sure the basic operator is still working */
+  testBasicOperator(cal, updateResult);
+
 });
